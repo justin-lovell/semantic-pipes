625920930d96a2eee3b12a6389b5339ace35752f
Justin Lovell <justin@justjuzzy.com>
2014-05-01 15:35:59 +0200

    processing of the callback from the pipe option validated
    we will validate that the output of the pipeline package.

--- SemanticPipes/PipePackageOption.cs
+++ SemanticPipes/PipePackageOption.cs
@@ -4,19 +4,47 @@ namespace SemanticPipes
 {
     public class PipePackageOption
     {
-        private readonly Func<object> _processCallbackFunc;
+        private readonly Func<object, object> _processCallbackFunc;
 
-        public PipePackageOption(Type outputType, Func<object> processCallbackFunc)
+        public PipePackageOption(Type outputType, Func<object, object> processCallbackFunc)
         {
-            this._processCallbackFunc = processCallbackFunc;
+            if (outputType == null) throw new ArgumentNullException("outputType");
+            if (processCallbackFunc == null) throw new ArgumentNullException("processCallbackFunc");
+
+            _processCallbackFunc = processCallbackFunc;
             OutputType = outputType;
         }
 
         public Type OutputType { get; private set; }
 
-        public Func<object> ProcessInput(object input)
+        public object ProcessInput(object input)
         {
-            throw new NotImplementedException();
+            if (input == null) throw new ArgumentNullException("input");
+
+            object output = _processCallbackFunc(input);
+
+            GuardAgainstNullResultFromCallback(output);
+            GuardAgainstUnexpectedReturnTypeFromCallback(output);
+
+            return output;
+        }
+
+        private void GuardAgainstUnexpectedReturnTypeFromCallback(object output)
+        {
+            Type outputType = output.GetType();
+            if (outputType == OutputType) return;
+
+            string message = string.Format("Callback returned '{0}'. We were expecting an object of type '{1}'.",
+                outputType, OutputType);
+            throw new NotSupportedException(message);
+        }
+
+        private static void GuardAgainstNullResultFromCallback(object output)
+        {
+            if (output != null) return;
+
+            const string message = "The output of the process callback returned null. We expected a non-null object.";
+            throw new NotSupportedException(message);
         }
     }
 }
---
+++ Test/PipePackageOptionTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SemanticPipes
+{
+    [TestFixture]
+    public class PipePackageOptionTests
+    {
+        [Test]
+        public void Ctor_WhenNullToOutputTypeParameter_ItShouldThrowArgumentNullExcpetion()
+        {
+            var argumentNullException = Assert.Throws<ArgumentNullException>(() =>
+            {
+                Func<object, object> processCallbackFunc = input => null;
+                var pipePackageOption = new PipePackageOption(null, processCallbackFunc);
+
+                Assert.Fail();
+            });
+
+            Assert.AreEqual("outputType", argumentNullException.ParamName);
+        }
+
+        [Test]
+        public void Ctor_WhenNullToProcessCallbackFuncParameter_ItShouldThrowArgumentNullExcpetion()
+        {
+            var argumentNullException = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var pipePackageOption = new PipePackageOption(typeof (string), null);
+                Assert.Fail();
+            });
+
+            Assert.AreEqual("processCallbackFunc", argumentNullException.ParamName);
+        }
+
+        [Test]
+        public void ProcessInput_WhenNullToInputParameter_ItShouldThrowArgumentNullException()
+        {
+            Func<object, object> processCallbackFunc = input => null;
+            var pipePackageOption = new PipePackageOption(typeof (string), processCallbackFunc);
+
+            var argumentNullException = Assert.Throws<ArgumentNullException>(() => pipePackageOption.ProcessInput(null));
+
+            Assert.AreEqual("input", argumentNullException.ParamName);
+        }
+
+        [Test]
+        public void ProcessInput_WhenProcesCallbackEchoesInput_ItShouldReturnTheInput()
+        {
+            Func<object, object> processCallbackFunc = input => input;
+            var pipePackageOption = new PipePackageOption(typeof (string), processCallbackFunc);
+
+            var output = pipePackageOption.ProcessInput("abc");
+
+            Assert.AreEqual("abc", output);
+        }
+
+        [Test]
+        public void ProcessInput_WhenProcesCallbackReturnsWhatIsNotTheExpectedOutputType_ItShouldThrowNotSupportedException()
+        {
+            Func<object, object> processCallbackFunc = input => 2;
+            var pipePackageOption = new PipePackageOption(typeof(string), processCallbackFunc);
+
+            Assert.Throws<NotSupportedException>(() => pipePackageOption.ProcessInput("abc"));
+        }
+
+        [Test]
+        public void ProcessInput_WhenProcesCallbackReturnsNull_ItShouldThrowNotSupportedException()
+        {
+            Func<object, object> processCallbackFunc = input => null;
+            var pipePackageOption = new PipePackageOption(typeof (string), processCallbackFunc);
+
+            Assert.Throws<NotSupportedException>(() => pipePackageOption.ProcessInput("abc"));
+        }
+    }
+}
