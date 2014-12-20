4e2f349e635ea5f417ea6609b3619ae481584efc
Justin Lovell <justin@justjuzzy.com>
2014-12-20 20:51:28 +0200

    renaming BuilderPipeAddOnForCollectionsTests to denote behavior
    working on gh-21

---
+++ Test/Feature_AutoMappingObjectsToCollectionObjects_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SemanticPipes
+{
+    [TestFixture]
+    public class Feature_AutoMappingObjectsToCollectionObjects_Tests
+    {
+        private sealed class TestObjectA
+        {
+        }
+
+        private sealed class TestObjectB
+        {
+        }
+
+        [Test]
+        public void GivenTwoClasses_WhenSolvingSingleClassToCollection_ItShouldResolve()
+        {
+            // pre-arrangement
+            var expectedReturnObject = new TestObjectB();
+
+            // arrange
+            var semanticBuilder = new SemanticBuilder();
+            semanticBuilder.InstallPipe<TestObjectA, TestObjectB>(source => expectedReturnObject);
+
+            ISemanticBroker semanticBroker = semanticBuilder.CreateBroker();
+
+            // act
+            var enumerable = semanticBroker.On(new TestObjectA())
+                .Output<IEnumerable<TestObjectB>>();
+
+
+            // assert
+            TestObjectB returnedType = enumerable.Single();
+            Assert.AreSame(expectedReturnObject, returnedType);
+        }
+    }
+}
