f91f6f3b4b09fba77fe66cb6a217213cb0b49992
Justin Lovell <justin@justjuzzy.com>
2014-12-21 14:15:57 +0200

    finally got rid of the PipeExtension class
    gh-20

--- SemanticPipes/SemanticBuilder.cs
+++ SemanticPipes/SemanticBuilder.cs
@@ -1,31 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SemanticPipes
 {
     public sealed class SemanticBuilder
     {
-        private readonly Dictionary<InputOutputPair, PipeExtension> _installedPipes =
-            new Dictionary<InputOutputPair, PipeExtension>();
-
         private readonly RegistryMediator _registryMediator =
             new RegistryMediator(SemanticRegistryObserverFactory.CreateInternalObservers());
 
-//        private IEnumerable<PipeExtension> IterateCurrentPipeExtensions()
-//        {
-//            return _installedPipes.Select(installedPipe => installedPipe.Value);
-//        }
 
         public ISemanticBroker CreateBroker()
         {
             var solver = _registryMediator.CreateSolver();
-            //IEnumerable<PipeExtension> currentPipeExtensions = IterateCurrentPipeExtensions();
-
-//            foreach (PipeExtension currentPipeExtension in currentPipeExtensions)
-//            {
-//                solver.Install(currentPipeExtension);
-//            }
 
             return new Broker(solver);
         }
@@ -40,29 +25,7 @@ namespace SemanticPipes
             PipeOutputPackage package = CreatePipeOutputPackage(processCallback);
             _registryMediator.AppendPackage(package);
 
-
-            var inputOutputPair = new InputOutputPair(typeof (TSource), typeof (TDestination));
-
-            PipeExtension extension = CreatePipeExtension(processCallback);
-
-
-            _installedPipes.Add(inputOutputPair, extension);
-
-
-
             return this;
-        }
-
-        private static PipeExtension CreatePipeExtension<TSource, TDestination>(
-            Func<TSource, TDestination> processCallback)
-        {
-            Func<object, object> wrappedProcessCallback = rawInput =>
-            {
-                var castedInput = (TSource) rawInput;
-                return processCallback(castedInput);
-            };
-            var extension = new PipeExtension(typeof (TSource), typeof (TDestination), wrappedProcessCallback);
-            return extension;
         }
 
         private static PipeOutputPackage CreatePipeOutputPackage<TSource, TDestination>(
@@ -86,13 +49,6 @@ namespace SemanticPipes
             }
 
             _registryMediator.AppendObserver(observer);
-        }
-
-        private sealed class InputOutputPair : Tuple<Type, Type>
-        {
-            public InputOutputPair(Type item1, Type item2) : base(item1, item2)
-            {
-            }
         }
     }
 }
