b85430b651d36ea23169bfda963fa8512123800e
Justin Lovell <justin@justjuzzy.com>
2014-12-26 11:50:32 +0200

    test for gh-4

---
+++ Test/Feature_AutoMappingCollectionToCollection_Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SemanticPipes
+{
+    [TestFixture]
+    public class Feature_AutoMappingCollectionToCollection_Tests
+    {
+        private sealed class TestObjectA
+        {
+        }
+
+        private sealed class TestObjectB
+        {
+        }
+
+        [Test]
+        public void GivenMappingFromObjectAToObjectB_WhenSolvingCollectionToCollection_ItShouldResolve()
+        {
+            // pre-arrangement
+            int counter = 0;
+            var expectedReturnObject = new TestObjectB();
+
+            // arrange
+            var semanticBuilder = new SemanticBuilder();
+            semanticBuilder.InstallPipe<TestObjectA, TestObjectB>(source =>
+            {
+                counter++;
+                return expectedReturnObject;
+            });
+
+            ISemanticBroker semanticBroker = semanticBuilder.CreateBroker();
+
+            // act
+            var enumerable = semanticBroker.On(new[] {new TestObjectA()})
+                .Output<IEnumerable<TestObjectB>>();
+
+
+            // assert
+            Assert.AreEqual(1, counter);
+            TestObjectB returnedType = enumerable.Single();
+            Assert.AreSame(expectedReturnObject, returnedType);
+        }
+    }
+}
