398693af983855c92f2c9828a74db742b08ada9c
Justin Lovell <justin@justjuzzy.com>
2014-12-27 19:27:29 +0200

    the nuget works
    closes gh-19

---
+++ NuGetPack.bat
@@ -0,0 +1,1 @@
+.\.nuget\NuGet.exe pack .\SemanticPipes\SemanticPipes.csproj -Prop Configuration=Release
--- SemanticPipes/Properties/AssemblyInfo.cs
+++ SemanticPipes/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SemanticPipes")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Justin Lovell 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.*")]
+[assembly: AssemblyFileVersion("0.9.*")]
